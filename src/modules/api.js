85c8952058d1c833e0e54525ec11a90c4cfc76b5
GeekyHacks <geekyhacks22@gmail.com>
2023-08-04 04:14:19 +0300

    Add

--- src/modules/api.js
+++ src/modules/api.js
@@ -31,3 +31,17 @@ export const getScore = async () => {
   const dataList = await response.json();
   return dataList;
 };
+
+
+// fetch(Url, {
+//   method: 'POST',
+//   body: JSON.stringify({
+//     name: 'GeekyHacks'
+//   }),
+//   headers: {
+//     'Content-type': 'application/json; charset=UTF-8',
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+// const gameID = '9W40RRKEzdVGseFky5fH';
